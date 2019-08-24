7dc8a5415567fe7def46d207b6389fa2459561d0
theXtroyer1221 <50244210+theXtroyer1221@users.noreply.github.com>
2019-08-24 21:07:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+python = True
+
+if (python):
+
+ x = ""
+
+ print("hej vad heter du")
+
+ x = input(x)
+
+ print("hej" + " " + x + " " + "hur mår du")
+
+if (x == "jad"):
+ #taking a dynamic input value
+ print ("vi alla vet jad är bäst, kan du snälla ge mig ett siffra",)
+ response = input()
+
+ number = int(response) 
+ plusTen = number + 10 
+ plusTwenty = number + 20
+
+ print ("om vi addar 10 till din siffra, vi får " + str(plusTen))
+ 
+ print ("om vi addar 20 till din siffra, vi får " + str(plusTwenty))
+
+
+
